63b6764402824b7236a800ee6d3e799be80cc725
Ang Chen <chenang@outlook.com>
2024-10-15 16:15:52 +0800

    Update src/components/layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,24 @@ import {
   heading,
   navLinks,
   navLinkItem,
-  navLinkText
+  navLinkText,
+  siteTitle
 } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+    const data = useStaticQuery(graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `)
+
   return (
     <div className={container}>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
