cade8ac78d7007c4cc2dddd660be87fb8bd0ed9c
Ang Chen <chenang@outlook.com>
2024-10-20 16:36:10 +0800

    Update src/components/layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -34,6 +34,11 @@ const Layout = ({ pageTitle, children }) => {
             <Link to="/about" className={navLinkText}>
             About
             </Link>
+            </li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+            Blog
+            </Link>
           </li>
         </ul>
       </nav>
